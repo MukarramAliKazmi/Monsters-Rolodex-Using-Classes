e43a6c8c388aa5d3005f51dc869a52994ed69293
Mukarram Kazmi <muazzam.ali.kazmi@gmail.com>
2022-08-09 23:54:36 +0500

    code optimizations

--- src/App.js
+++ src/App.js
@@ -26,26 +26,29 @@ class App extends Component {
       ));
   }
 
+  onSearchChange = (event) => {
+    console.log(event.target.value)
+    const searchField = event.target.value.toLocaleLowerCase()
+
+    this.setState(() => {
+      console.log(this.state.monsters)
+      return { searchField }
+    })
+  };
+
   render() {
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
     })
 
     return (
       <div className="App">
         <input className="search-box" type="search" placeholder="search monsters"
-          onChange={
-            (event) => {
-              console.log(event.target.value)
-              const searchField = event.target.value.toLocaleLowerCase()
-
-              this.setState(() => {
-                console.log(this.state.monsters)
-                return { searchField }
-              })
-            }
-          }
+          onChange={onSearchChange}
         />
 
         {filteredMonsters.map((monster) => {
