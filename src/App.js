a3a0a9f8d19b805dce1d3f1b40bbcfe6ae624110
Mukarram Kazmi <muazzam.ali.kazmi@gmail.com>
2022-08-10 18:31:42 +0500

    added card-list component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+
+import CardList from './components/card-list/card-list-component';
 import './App.css';
-import { render } from '@testing-library/react';
 
 class App extends Component {
   constructor() {
@@ -19,19 +19,14 @@ class App extends Component {
       .then((users) => this.setState(
         () => {
           return { monsters: users };
-        },
-        () => {
-          console.log(this.state);
         }
       ));
   }
 
   onSearchChange = (event) => {
-    console.log(event.target.value)
     const searchField = event.target.value.toLocaleLowerCase()
 
     this.setState(() => {
-      console.log(this.state.monsters)
       return { searchField }
     })
   };
@@ -47,13 +42,13 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input className="search-box" type="search" placeholder="search monsters"
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
           onChange={onSearchChange}
         />
-
-        {filteredMonsters.map((monster) => {
-          return <div key={monster.id}><h1>{monster.name}</h1></div>
-        })}
+        <CardList monsters={filteredMonsters} />
       </div >
     );
   }
